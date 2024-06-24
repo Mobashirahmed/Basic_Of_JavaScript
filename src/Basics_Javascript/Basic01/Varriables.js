8fed824206a9bd694361943acaf1dc585ffe3597
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-06-24 08:54:13 +0000

    learning variables

---
+++ src/Basics_Javascript/Basic01/Varriables.js
@@ -0,0 +1,22 @@
+const accountId = 3141459265
+let accountEmail = "mobashir@google.com"
+var accountPassword = 66260755
+accountCity = " Muradabad"
+let accountState
+
+
+//accountId = 65425//
+
+accountEmail="Mark@bk.com"
+accountPassword="7554r$"
+acoountCity="Dishpur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+
+/*
+   Prefer not to use var
+   cuz there'll be issues with block scope and functional scope
+   The Symbol for block scope is currly bracess {}
+*/
