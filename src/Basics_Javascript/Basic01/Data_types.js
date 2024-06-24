d47441fa9d0de4ccf06f7e77cf4ef70fcb660ef9
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-06-24 11:59:58 +0000

    Num & String

---
+++ src/Basics_Javascript/Basic01/Data_types.js
@@ -0,0 +1,25 @@
+"use strict";// treat all JS code as newer version
+
+//alert("67+43") // we are using node JS not Browser
+
+console.log(8+76);
+// code readibility should be high
+console.log("Shaikh Siddique")
+let name="Mobashir"
+let age=18
+let isLoggedIn= true
+let state;
+
+// number= 2 to the power 45
+// bigint
+// boolean = true/false
+// null => standalone value
+// undefined => value hasn't assigned yet
+// symbol =. unique
+
+//object
+
+console.log(typeof"Syed Ahmad Khan")
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
