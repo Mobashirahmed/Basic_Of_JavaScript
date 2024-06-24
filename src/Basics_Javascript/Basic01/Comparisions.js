5e6514fb3bad1b29343f19b44d716ff4d9a5f823
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-06-24 11:59:11 +0000

    truth value of comparisons

---
+++ src/Basics_Javascript/Basic01/Comparisions.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(6<7);
+console.log( 7==45 );
+console.log( 2 === 2);
+
+console.log("56" >23);
+console.log("078"< 98);
+
+
+/*************************** Prohibited Ones ***************************/
+// console.log(null > 0);
+// console.log(null >=0);
+// console.log(null ==);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// strictcheck === : tripple assignment checks the equality of values as well as its data type
+console.log("54" === 54);
