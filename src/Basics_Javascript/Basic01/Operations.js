7d033d92dc95e62428bf15c82a216d99f1480b27
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-06-24 11:35:02 +0000

    learnt Operations

---
+++ src/Basics_Javascript/Basic01/Operations.js
@@ -0,0 +1,36 @@
+let value =3
+let negValue = -value
+console.log(negValue);
+
+// console.log(); //aditinon
+// console.log(); //sub
+// console.log(); //single stared
+// console.log(); // double stared
+// console.log();
+// console.log();
+
+let str1 ="Hello"
+let str2 ="EveryOne"
+let str3 =str1+str2
+console.log(str3);
+
+console.log(45+83);
+console.log("54"+62);  //double sum when one of the operand is num and other is string no summation of values takes place
+console.log(97+"43");
+console.log(62+78+"56"); //triple sum string [first and string last or vice versa] here the numeral values get added but hte string value comes out as it is.
+console.log("84"+93+65);  // here in case of string first; the value gets printed as it is
+
+// console.log(True);
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3= 2 + 45
+console.log(num3);
+
+let gameCounter = 100
+gameCounter++
+++gameCounter
+// Hitesh bhaiya says Operators precedence is only for Exams , those articles aren't significant for production. And prefix and postfix operators
