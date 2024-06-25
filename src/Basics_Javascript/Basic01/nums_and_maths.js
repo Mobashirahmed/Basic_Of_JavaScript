7e88d24b1fa6719f384d2049ef21987c0a0737a4
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-06-25 09:02:13 +0000

    properties of maths funcs.

---
+++ src/Basics_Javascript/Basic01/nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 343
+console.log(score);
+
+const balance = new Number (343)
+console.log(balance);
+
+
+console.log(balance.toString(), length);
+console.log(balance.toFixed(2));
+
+const anaothernumber = 3246.53
+console.log(anaothernumber.toPrecision(4));
+
+const hundreds = 33324
+console.log(hundreds.toLocaleString(`en-IN`));
+
+// *************************MATHS**************************//
+
+console.log(Math);
+console.log(Math.abs(-65));
+console.log(Math.round());
+console.log(Math.ceil());
+console.log(Math.floor());
+console.log(Math.max());
+console.log(Math.min);
+// put values in them
+
+console.log(Math.floor(Math.random())); // this will random values from 0 to 1 and floor will behave as GIFunction.
+console.log(Math.random());
+
+console.log(Math.random()*43+1);
+console.log(Math.random()*43+1);
+console.log(Math.random()*43+1);
+
+
+const min = 10
+const max = 45
+
+console.log(Math.floor(Math.random() * (max - min +1) + min ));
