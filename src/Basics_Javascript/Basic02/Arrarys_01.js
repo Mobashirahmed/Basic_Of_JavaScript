f401e8082aa167f1f6907fbc84f80756459f85de
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-07-01 15:22:57 +0000

    arrays

--- src/Basics_Javascript/Basic02/Arrarys_01.js
+++ src/Basics_Javascript/Basic02/Arrarys_01.js
@@ -1,45 +1,49 @@
 //Array
 
-const myArr1 = [7, 4, 3, 2, 6, 0, 5, 8,]
+const myArr1 = [7, 4, 6, 0, 5, 8,]
+console.log(myArr1[4]); // we cannot write arrays like "string"
+myArr1.push(9)
+console.log(myArr1.includes(9)); // .includes is a boolean method in JS that returns a T/F output
+console.log(myArr1.indexOf(3)); // value jo exist karti hai agar () ke andar entre karoge toh .indexOf returns its index no. That is similar to container[] writtten inside console.log()
+
 const myHeros = ["Shaktiman", "IronMan", "SpiderMan", "CaptainAmerica"]
+console.log(myHeros);
+
+const myArr2 = new Array(0, 1, 2, 3, 4) // yeh ek dusara tarika hai array assign karne ke liye
+console.log(myArr2[1]);
+console.log(myArr2);
+
+//Array methods
+// const arr01 = new Array(2, 3, 4, entre, "Afsana", 7, 4)
+// arr01.push(6) // this will add 6 as extreme entry
+// arr01.push(7)
+// arr01.pop() // this will remove the very last entry
+// console.log(arr01);
+// const arr02 = [8, 3, 5, play, "MyLove", 786, 64]
+// arr02.unshift(9) // this will add 9 as the very first entry
+// arr02.unshift(7)
+// arr02.shift() // this will remove the very first entry
+// console.log(arr02);
+
+const myArr3 = myArr1.join() // .join kya kar raha hai😥 Ans: It converts the data type of array from Object to String. In the output those square brackets were removed along with those spaces and the data in the array comes out as string.
 
 console.log(myArr1);
-console.log(myHeros);
-console.log(myArr1[0]); // we cannot write arrays like "string"
-
-// const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
-
-// //Array methods
-// myArrr.push(6)
-// myArr.push(6)
-// myArr.pop()
-// console.log(myArr);
-
-// myArr.unshift(9)
-// myArr.shift()
-
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3)); // value jo exist karti hai
-// console.log(myArr);
-
-// const myArr3 = myArr3.join()
-
-// console.log(myArr);
-// console.log(typeOf newArr3);
+console.log(myArr3);
+console.log(typeof myArr1);
+console.log(typeof myArr3);
 
 //slice, splice
 
-// const mobashirarray = [5, 7, 4, 5 , 7, 0, 3]
+const mobashirarray = [5, 7, 4, 5 , 7, 0, 3]
 
-// console.log("A",mobashirarray );
+console.log("A",mobashirarray );
 
-// const myn1 = mobashirarray.slice(1, 3)
+const myn1 = mobashirarray.slice(1, 3) // returs a copy of section of an array
 
-// console.log(myn1);
-// console.log("B", mobashirarray);
+console.log(myn1);
+console.log("B", mobashirarray);
 
-// const myn2 = mobashirarray.splice(1, 3)
-// console.log(myn1);
-// console.log(myn2);
-// console.log("c", mobashirarray); 
+const myn2 = mobashirarray.splice(1, 3)
+console.log("C", mobashirarray);
+console.log(myn2);
+console.log(myn1);
