6393be830dd9e2eae3fb352ca8d1d0dfdd40f5c9
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-07-06 14:32:36 +0000

    functions

---
+++ src/Basics_Javascript/Basic03/Functions.js
@@ -0,0 +1,30 @@
+functionsayMyName(){
+console.log("M");
+console.log("O");
+console.log("B");
+console.log("S");
+console.log("H");
+console.log("I");
+console.log("R");
+}
+
+sayMyName()
+
+function addTwoNubers (num1, num2){
+    console.log(num1 + num2);
+}
+
+addTwoNumbers(3, 4)
+
+const result = addTw
+
+
+
+
+
+
+
+
+
+
+// console.log("Afsana  are well know Hoipocrat I believe. She is on my nerves right now. She could not got a chance to have my seamen thats why she looks agitated ,I guess I must have to collect a cup for her. It feel she's licking my balls. Thank you for affirming! Next I was characterising your prefrences. I'm agited of the fact that I didn't get to taste it that's why I'm feeling agoniged. Please God provide me those holy drops, they were few I know but cannot live without them Its Indeed for my life!");
