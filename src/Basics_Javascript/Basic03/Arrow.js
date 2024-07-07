c10cb750739fce6e30df5a95152e931f99fe90bc
Mobashir Ahmed <140891242+Mobashirahmed@users.noreply.github.com>
2024-07-07 14:11:32 +0000

    Arrow Functions

--- src/Basics_Javascript/Basic03/Arrow.js
+++ src/Basics_Javascript/Basic03/Arrow.js
@@ -3,50 +3,53 @@ const User = {
     username: "Mobashir",
     price: 999.00,
 
-    welcomeMesssage: function() {
+    welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`); // .this ka matalab hai current context jo iss curly braces ke undar hai
         console.log(this);
     }
 
 }
 
-// user.welcomeMessage()
-// user.username = "Sam"
-// usee.welcomeMessage()
+User.welcomeMessage()
+User.username = "Sam" // Here,We'd changed the context/value of username. Now this User will be welcomed
+User.welcomeMessage() // & this line will Execute the changed context result
 
-console.log(this);
+console.log(this); // this will return an empty object
 
-// function afsana(){
-//     let Loves = "Mobashir"
-//     console.log(this.username);
-// }
-
-// chai()
-
-const coffee =() => { 
-    let username = "Afsana"
-    console.log(this);
+function afsana(){
+    // console.log(this); // Here We're printing this inside a node Environment
+    let Loves = "Mobashir"
+    console.log(this.username);
+    // console.log(this);
 }
 
-coffee()
+afsana()
+
+// const coffee =() => { 
+//     let username = "Afsana"
+//     console.log(this);
+// }
+
+// coffee()
 // arrow function ke app yeh this use nahi kar sakte aur regular funstions me aap use kar sakte hain , Generally diff. karne waqt yahi reply aata hai
 
 
 //Arrow Functions ke bare me aur Discussion
 
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-// console.log(addTwo(3, 4));
+const addTwo = (num1, num2) => {
+    return num1 + num2 // yeh explicit return hai
+}
+console.log(addTwo(3, 4)); // Here Arrow Function ko call karne bhar se print nahi ,One have to use console.log to print this.
 
-// const addTwo = (num1, num2) => num1 + num2
-// console.log(addTwo(3, 4));
+const sumTwo = (num1, num2) => num1 + num2 // yeh implicit return hai yaha humne parentheses ka upyog nahi kiya
+console.log(sumTwo(7, 6));
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
-// console.log(addTwo(3, 4));
-// const addTwo = (num1, num2) => ({username: "afsana"})
-// console.log(addTwo(3, 4));
+const sumofTwo = (num1, num2) => ( num1 + num2 )
+console.log(sumofTwo(5, 3));
 
-const myArray = [2, 3, 6, 7 ,3 ,8 ,9]
+const addingTwonumbers = (num1, num2) => ({username: "afsana"}) // benefit of parentheses
+console.log(addingTwonumbers(3, 4));
 
-myArray.forEach(() => ())
+// const myArray = [2, 3, 6, 7 ,3 ,8 ,9]
+
+// myArray.forEach()
--- src/Basics_Javascript/Basic03/Scopes.js
+++ src/Basics_Javascript/Basic03/Scopes.js
@@ -41,33 +41,35 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
-for (let i = 0; i <array.length;i++) {
-    const element = array[i];
+// ************** Next Tutorial ******************
 
-}
+// for (let i = 0; i <array.length;i++>){
+//     const element = array[i];
 
-console.log(a);
-console.log(b);
-console.log(c);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
 function one(){
-    const Programmer =  "Mobashir"
-    function twi(){
+    const Programmer = "Mobashir"
+    function two(){
         const website = "Youtube"
-        console.log(username);
+        console.log(Programmer); // yahan chhota bhai apne bade bhai se Ice-Creme le kar kha raha hai
     }
-    // console.log(website); yaha erro hoga
+    // console.log(website); yaha erro hoga cuz: Block scope ke andar ki chiz ko bahar acces kar rahe ho
 
-    two()
+    two() // yahan hum function two ko execute kar rahe hain Execution line scope ke humesha bahar hi likhte hain lekin vah kisi bade Block Scope ke andar nested bhi ho sakta hai jaisa ki yahan hai
 }
 
-one()
+one() // yaha function one execute ho raha hai what is being signified by empty parenthese ()
 
 if (true) {
     const username = "Mobashir"
     if (username === "Mobashir") {
         const website = " mobsite"
-        console.log(username + website);
+        console.log(username + website); // yeh line likhne ke baad output print ho raha hai
     }
     // console.log(website);
 }
@@ -76,15 +78,21 @@ if (true) {
 
 // ++++++++++++++++++++++++++ Intresting ++++++++++++++++++++++++++
 
-console.log(addone(5))
-
+// yeh Function hai.
+console.log(addone(8))
+// addone(73)
 function addone(num){
     return num + 1
 }
 
+// addone(63)
 
+// yeh Expression hai
 const addTwo = function(num){
     return num + 2
 }
 
-addTwo(5)
+addTwo(4)
+
+// Above was a Directly declared Function and the one below it is a contained Function
+//We cannot execute or call a contained Function before its Lines. But we can call a Non-contained Function anytime we want, the only thing where we must pay attention to is that call must be written outside its scope
